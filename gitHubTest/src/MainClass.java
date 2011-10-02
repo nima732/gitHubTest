5a973d8e2ea9d0468532ee8b490639228d9dc325
Nima <nima7321@gmail.com>
2011-10-02 23:56:53 +0200

    nima2

--- gitHubTest/src/MainClass.java
+++ gitHubTest/src/MainClass.java
@@ -2,5 +2,7 @@
 public class MainClass {
 	public static void main(String args[]){
 		System.out.println("ooookkkkoooooooo");
+		System.out.println("ooookkkkoooooooo");
+		System.out.println("ooookkkkoooooooo");
 	}
 }
